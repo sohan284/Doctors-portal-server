f928ab41411f21742bfcf4ab4f0e79cb4f2fdbf1
srsohan284 <srsohan284.com>
2022-05-21 00:51:03 +0600

    available

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+
 const express = require('express');
-require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jdute.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,15 +27,47 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             const services = await cursor.toArray();
             res.send(services);
         })
-        app.post('/booking', async(req, res) =>{
-         const booking = req.body;
-         const query = {treatment:booking.treatment,date:booking.date ,patient:booking.patient}
-        const exists = await bookingCollection.insertOne(query);
-        if(exists){
-          return res.send({success:false,booking:exists})
-        }
-         const result  = await bookingCollection.insertOne(booking)
-         return res.send({success:true,result})
+
+        app.get('/available',async(req,res)=>{
+          const date = req.query.date || 'May 20, 2022';
+
+          const services = await serviceCollection.find().toArray();
+
+          const query = {date:date};
+          const bookings = await bookingCollection.find(query).toArray();
+
+          services.forEach(service=>{
+            const serviceBookings = bookings.filter(book => book.treatment === service.name);
+            
+            const bookedSlots = serviceBookings.map(book => book.slot);
+
+            const available = service.slots.filter(slot => !bookedSlots.includes(slot));
+
+            service.slots = available;
+          });
+
+
+
+          res.send(services);
+
+        })
+
+        app.get('/booking', async(req, res) =>{
+          const patient = req.query.patient;
+          const query = {patient: patient};
+          const bookings = await bookingCollection.find(query).toArray();
+          res.send(bookings);
+        })
+
+        app.post('/booking', async (req, res) => {
+          const booking = req.body;
+          const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+          const exists = await bookingCollection.findOne(query);
+          if (exists) {
+            return res.send({ success: false, booking: exists })
+          }
+          const result = await bookingCollection.insertOne(booking);
+          return res.send({ success: true, result });
         })
 
      }
@@ -42,7 +76,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
      }
 
  }
- run().catch(console.dir)
+ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
